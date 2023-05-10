f880b746246122f7c9eb80af7f5d65c76f5bdff5
RameshKumal <rameshkc498@gmail.com>
2023-05-10 11:53:00 +0530

    initial commit

---
+++ server/FakeData.js
@@ -0,0 +1,49 @@
+exports.userList = [
+  {
+    id: 1,
+    name: "John Doe",
+    username: "johndoe123",
+    age: 27,
+    nationality: "American",
+  },
+
+  {
+    id: 2,
+    name: "Maria Rodriguez",
+    username: "mariarodz",
+    age: 34,
+    nationality: "Mexican",
+  },
+
+  {
+    id: 3,
+    name: "Hassan Ali",
+    username: "hassanali",
+    age: 22,
+    nationality: "Pakistani",
+  },
+
+  {
+    id: 4,
+    name: "Sophie Johnson",
+    username: "sophiej",
+    age: 41,
+    nationality: "British",
+  },
+
+  {
+    id: 5,
+    name: "Ahmed Hassan",
+    username: "ahmedhassan",
+    age: 29,
+    nationality: "Egyptian",
+  },
+
+  {
+    id: 6,
+    name: "Lena Kim",
+    username: "lenak",
+    age: 25,
+    nationality: "South Korean",
+  },
+];
